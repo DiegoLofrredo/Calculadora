eebd11b489c3d3adb0491a6d0f2b737b7a04a630
Diegoloffredo <diegoloffredobatista@hotmail.com>
2020-03-03 17:39:21 -0300

    Adicionado as funções de multiplicação e divisão

--- Calculadora.java
+++ Calculadora.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 	
 	Scanner entrada = new Scanner(System.in);
 	
-	System.out.print("Digite a operação desejada (+ ou -): ");
+	System.out.print("Digite a operação desejada (+, -, *, /): ");
 	operacao = entrada.nextLine();
 	
 	System.out.println("Digite um valor: ");
@@ -27,6 +27,16 @@ import java.util.Scanner;
    if(operacao.equals("-")) {
 		System.out.println("O resultado da subtração é: " +(n1 - n2));
 		control++;		
+	}
+   
+   if(operacao.equals("*")) {
+		System.out.println("O resultado da multiplicação é: " +(n1 * n2));
+		control++;			
+	}
+	
+	if(operacao.equals("/")) {
+		System.out.println("O resultado da divisão é: " +(n1 / n2));
+		control++;			
 	}
 	
 	if(control == 0) {
